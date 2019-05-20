3f1ba556d8910fa32e9f69da0f1b36925dbbff63
swcmdb <48962272+swcmdb@users.noreply.github.com>
2019-05-20 11:54:26 -0700

    Delete run.sh

--- docker/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker load < splunk_save.tar
-docker load < metabase_save.tar
-docker load < mongo_save.tar
-docker load < python_save.tar
-docker load < tomcat_save.tar
-docker load < pyflow_save.tar
-docker-compose up -d
